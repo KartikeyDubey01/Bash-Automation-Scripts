b833e1d2931baa71afa6946e6e11754719beb44f
Kartikey Dubey <kartikeykv2@gmail.com>
2025-03-09 22:55:13 +0530

    Shell Script

---
+++ Basic_Info.sh
@@ -0,0 +1,7 @@
+echo '------System Information------'
+echo "Current Date and Time: $(date)"
+echo "Username: $(whoami)"
+echo "Hostname: $(hostname)" 
+echo "System Uptime: $(uptime -p)"
+echo "Operating System: $(uname -o)"
+echo "Kernel Version: $(uname -r)"
